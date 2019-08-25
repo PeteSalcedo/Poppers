024829f727b513486b28c9e94fc55bb4d8da6762
PeteSalcedo <peter.salcedo.diaz@gmail.com>
2019-08-24 23:03:49 -0500

    Props being passed down to the individual actor cards

--- src/components/elements/Actor/Actor.js
+++ src/components/elements/Actor/Actor.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { IMAGE_BASE_URL } from '../../../config'
-import 'Actor.css'
+import './Actor.css'
 
 const Actor = (props) => {
+    const POSTER_SIZE ="w154";
     return(
-        <div>
-            Actor
+        <div className="rmdb-actor">
+            <img 
+            src={props.actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${props.actor.profile_path}`: './image/no_image.jpg'} alt={props.character}></img>
+            <span className="rmdb-actor-name">{props.actor.name}</span>
+            <span className="rmdb-actor-character">{props.actor.character}</span>
         </div>
     )
 }
--- src/components/elements/MovieInfoBar/MovieInfoBar.js
+++ src/components/elements/MovieInfoBar/MovieInfoBar.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import FontAwsome from 'react-fontawesome'
-import { CalcTime,convertMoney} from '../../../helpers'
+import { calcTime,convertMoney,} from '../../../helpers'
 import './MovieInfoBar.css'
 
 const MovieInfoBar = (props) => {
     return(
-        <div>
-            MovieInfoBar
+        <div className="rmdb-movieinfobar">
+        <div className="rmdb-movieinfobar-content">
+        <div className="rmdb-movieinfobar-content-col">
+            <FontAwsome className="fa-time" name="clock-o" size="2x"></FontAwsome>
+            <span className="rmdb-movieinfobar-info">Runing time:{calcTime(props.time)}</span>
+        </div>
+        <div className="rmdb-movieinfobar-content-col">
+
+            <FontAwsome  className="fa-budget" name="money" size="2x" />
+            <span className="rmdb-movieinfobar-info">Budget:{convertMoney(props.budget)}</span>
+        </div>
+        <div className="rmdb-movieinfobar-content-col">
+            <FontAwsome  className="fa-revenue" name="ticket" size="2x" />
+            <span className="rmdb-movieinfobar-info">Reveue:{convertMoney(props.revenue)}</span>
+        </div>
+        </div>
         </div>
     )
 }
